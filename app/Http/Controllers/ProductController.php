41812a3f1aa52780c0fde9a4d684289c4758fab0
Ghaidah Muthmainnah <ghaidahmuthmainnah4@gmail.com>
2026-06-04 23:57:32 +0800

    feat: implementasi fitur read untuk Product dengan menampilkan data relasi dari Category beserta fitur searching, pagination, dan filter

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,79 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        $search = $request->search;
+        $category = $request->category;
+
+        $product = Product::with('category');
+
+        if ($search) {
+            $product->where('nama_produk', 'like', '%' . $search . '%');
+        }
+        if ($category) {
+            $product->where('category_id', $category);
+        }
+        return view('products.index', [
+            'title' => 'Product',
+            'products' => $product
+                ->latest()->paginate(10)->withQueryString(),
+
+            'categories' => Category::all()
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Product $department) {}
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Product $department)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Product $department)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Product $department)
+    {
+        //
+    }
 }
---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,89 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="card shadow">
+
+    <div class="card-header d-flex justify-content-between align-items-center">
+        <h4 class="mb-0">Data Produk</h4>
+        <a href="{{ route('products.create') }}" class="btn btn-success">Tambah Produk</a>
+    </div>
+
+    <div class="card-body">
+
+        <form method="GET" action="{{ route('products.index') }}" class="row g-2 mb-3">
+
+            <div class="col-md-5">
+                <input type="text"
+                    name="search"
+                    class="form-control"
+                    placeholder="Cari Produk..."
+                    value="{{ request('search') }}">
+            </div>
+
+            <div class="col-md-4">
+                <select name="category" class="form-select">
+                    <option value="">Semua Kategori</option>
+
+                    @foreach($categories as $category)
+
+                    <option value="{{ $category->id }}"
+                        {{ request('category') == $category->id ? 'selected' : '' }}>
+
+                        {{ $category->nama_kategori }}
+
+                    </option>
+
+                    @endforeach
+
+                </select>
+
+            </div>
+
+            <div class="col-md-3">
+                <button class="btn btn-success">Filter</button>
+            </div>
+
+        </form>
+
+        <table class="table table-bordered table-striped">
+            <thead class="table-info">
+
+                <tr class="text-center">
+                    <th>No</th>
+                    <th>Nama Produk</th>
+                    <th>Kategori</th>
+                    <th>Harga</th>
+                    <th>Stok</th>
+                    <th>Aksi</th>
+                </tr>
+
+            </thead>
+            <tbody>
+
+                @forelse($products as $product)
+
+                <tr>
+                    <td>{{ $loop->iteration + ($products->firstItem() - 1) }}</td>
+                    <td>{{ $product->nama_produk }}</td>
+                    <td>{{ $product->category->nama_kategori }}</td>
+                    <td>Rp {{ number_format($product->harga) }}</td>
+                    <td>{{ $product->stok }}</td>
+                    <td class="text-center">-</td>
+                </tr>
+
+                @empty
+
+                <tr>
+                    <td colspan="6" class="text-center">Data tidak ditemukan</td>
+                </tr>
+
+                @endforelse
+
+            </tbody>
+        </table>
+        {{ $products->links() }}
+    </div>
+</div>
+
+@endsection
